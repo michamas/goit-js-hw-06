c85dbc68ab55c49552832b6b5a698433681158ea
Michał Masiulanis <m.masiulanis@gmail.com>
2022-12-06 20:17:38 +0100

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+/*
+Napisz skrypt, który dla każdego elementu tablicy ingredients:
+
+Utworzy oddzielny element <li>. Koniecznie użyj metody document.createElement().
+Doda nazwę elementu jako jego zawartość tekstową.
+Doda do elementu klasę item.
+Po czym umieści wszystkie <li> na liście podczas jednej operacji ul#ingredients.
+
+*/
+
+const findUl = document.querySelector("#ingredients");
+// console.log(findUl);
+
+const secondTask = ingredients.forEach((ingredient) => {
+  let ingredientList = document.createElement("li"); //Utworzy oddzielny element
+  ingredientList.textContent = ingredient; //Doda nazwę elementu jako jego zawartość tekstową.
+  ingredientList.classList.add("item"); // Doda do elementu klasę item.
+  findUl.append(ingredient);
+});
